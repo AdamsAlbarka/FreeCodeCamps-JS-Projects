d44746171ff07c69cae29fe369d0c2e49355f7e2
Adams Albarka <abdullahiadam@student.oauife.edu.ng>
2022-10-12 14:33:28 +0100

    FCC_React Map lesson 40

--- index_freecodecamp_tutorial.js
+++ index_freecodecamp_tutorial.js
@@ -128,3 +128,31 @@ class GateKeeper extends React.Component {
       );
     }
   }
+
+
+//  Map lesson 40
+
+const frontEndFrameworks = [
+    'React',
+    'Angular',
+    'Ember',
+    'Knockout',
+    'Backbone',
+    'Vue'
+  ];
+  
+  function Frameworks() {
+    const renderFrameworks = frontEndFrameworks.map(i => 
+    <li key={i}>
+    {i}
+    </li>
+    )
+    return (
+      <div>
+        <h1>Popular Front End JavaScript Frameworks</h1>
+        <ul>
+          {renderFrameworks}
+        </ul>
+      </div>
+    );
+  };
